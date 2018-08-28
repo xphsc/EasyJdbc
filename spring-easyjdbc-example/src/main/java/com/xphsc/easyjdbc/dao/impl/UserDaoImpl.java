f5e0c7438d6c1fd107443dd48a7b31a7422585a3
xphsc <xiongpeih@163.com>
2018-08-28 16:37:30 +0800

    Update UserDaoImpl.java

--- spring-easyjdbc-example/src/main/java/com/xphsc/easyjdbc/dao/impl/UserDaoImpl.java
+++ spring-easyjdbc-example/src/main/java/com/xphsc/easyjdbc/dao/impl/UserDaoImpl.java
@@ -3,7 +3,6 @@ package com.xphsc.easyjdbc.dao.impl;
 import com.xphsc.easyjdbc.core.SimpleJdbcDao;
 import com.xphsc.easyjdbc.dao.UserDao;
 import com.xphsc.easyjdbc.model.User;
-import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Repository;
 /**
  * @author huipei.x
